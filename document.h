c61aefcc8e964108b06c1f91ba1ba0bdc7c90135
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-06-30 13:05:03 +0300

    initial commit

---
+++ document.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+
+struct Document {
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+    
+    Document();
+    Document(int id, double relevance, int rating);
+};
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+//enum class DocumentStatus {
+//    kActual,
+//    kIrrelevant,
+//    kBanned,
+//    kRemoved,
+//};
+
+std::ostream& operator<<(std::ostream& out, const DocumentStatus status);
+
+void PrintDocument(const Document& document);
+
+std::ostream& operator<<(std::ostream& output, const Document& document);
+
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <iostream>
+
+#include "search_server.h"
+#include "request_queue.h"
+#include "paginator.h"
+#include "process_queries.h"
+
+//#include "test_search_server.hpp"
+
+using namespace std::literals;
+using namespace std;
+
+int main() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    id = 0;
+    for (
+        const auto& documents : ProcessQueries(search_server, queries)
+    ) {
+        cout << documents.size() << " documents for query ["s << queries[id++] << "]"s << endl;
+    }
+
+    return 0;
+} 
