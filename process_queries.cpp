c0e100eab35b5a56f477f20879e9f1e6687b35a1
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-07-01 11:03:26 +0300

    seems to work

--- process_queries.cpp
+++ process_queries.cpp
@@ -14,3 +14,18 @@ std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_ser
 
     return output;
 }
+
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server,
+                                           const std::vector<std::string>& queries) {
+        
+    const auto documents = ProcessQueries(search_server, queries);
+
+    const auto func = [](std::vector<Document> first, std::vector<Document> second){
+        for (const Document document : second) {
+            first.push_back(document);
+        }
+        return first;
+    };
+
+    return std::reduce(std::execution::par, documents.begin(), documents.end(), std::vector<Document>{}, func);
+}
--- process_queries.h
+++ process_queries.h
@@ -8,21 +8,8 @@
 #include "search_server.h"
 
 // this
-std::vector<std::vector<Document>> ProcessQueries(
-                                                  const SearchServer& search_server,
+std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_server,
                                                   const std::vector<std::string>& queries);
 
 std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server,
-                                           const std::vector<std::string>& queries) {
-        
-    const auto documents = ProcessQueries(search_server, queries);
-
-    const auto func = [](std::vector<Document> first, std::vector<Document> second){
-        for (const Document document : second) {
-            first.push_back(document);
-        }
-        return first;
-    };
-
-    return std::reduce(std::execution::par, documents.begin(), documents.end(), std::vector<Document>{}, func);
-}
+                                           const std::vector<std::string>& queries);
